5bb03384d4815836d78e9d6eee5eb24a24583cd3
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-09-18 15:58:49 +0300

    Updates

--- assets/status-tap-legacy-C9npjLJK.js
+++ assets/status-tap-legacy-C9npjLJK.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BPVNEs0X.js","./index8-legacy-CgHWCXvZ.js"],function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
+System.register(["./index-legacy-Qawq36XF.js","./index8-legacy-BIfyqb2S.js"],function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
