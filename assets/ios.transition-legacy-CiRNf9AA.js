52680cd27039db951ea0e77066df311d2c23f3ce
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-09-18 16:22:28 +0300

    Updates

--- assets/ios.transition-legacy-CiRNf9AA.js
+++ assets/ios.transition-legacy-CiRNf9AA.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BPVNEs0X.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.i,n=t.j}],execute:function(){
+System.register(["./index-legacy-Qawq36XF.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.i,n=t.j}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
