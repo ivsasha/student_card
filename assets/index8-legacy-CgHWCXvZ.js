26fe94d3741ee3b4ad9352a808f7b3d32da14717
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-09-18 15:52:02 +0300

    Updates

--- assets/index8-legacy-CgHWCXvZ.js
+++ assets/index8-legacy-CgHWCXvZ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Qawq36XF.js"],function(o,e){"use strict";var t;return{setters:[o=>{t=o.a}],execute:function(){
+System.register(["./index-legacy-BPVNEs0X.js"],function(o,e){"use strict";var t;return{setters:[o=>{t=o.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard-legacy-CXu6DENt.js
+++ assets/keyboard-legacy-CXu6DENt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Qawq36XF.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BPVNEs0X.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
